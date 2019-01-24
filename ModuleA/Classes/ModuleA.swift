6d19b98325b0a6d68b54c646e2c92d74c3ed9db5
shayuan <shayuan@silvrr.com>
2019-01-24 19:18:55 +0800

    m: update

--- ModuleA/Classes/ModuleA.swift
+++ ModuleA/Classes/ModuleA.swift
@@ -5,7 +5,7 @@ extension ModuleAMediatorType: MediatorSourceType {
     public var viewController: UIViewController? {
         switch self {
         case .home(let title):
-            let vc = ViewController()
+            let vc = UIViewController()
             vc.title = title
             return vc
             
